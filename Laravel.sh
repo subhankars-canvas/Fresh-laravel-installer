2a1652a3812147f5fabb8f325e550b15c0a88883
tier511 <work@tier5.us>
2016-06-24 20:39:08 +0530

    laravel installer file

---
+++ Laravel.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#install Laravel 4 or 5 according to need
+timestamp=$(date +%s)
+echo -n "Which version you wana install Laravel 4.* or 5.* tell me [ENTER]:"
+read version
+if [ -n "$version" ]; then
+	if(( ("$version" <= 4) )); then
+		echo "well sudipta I m smart enough to find ur name as well"
+		echo "I m gonna install laravel 4.2 in ur fucking machine"
+		echo -n "what should be the project folder name? [ENTER] :"
+		read folder_name
+		cd /var/www/html
+		composer create-project laravel/laravel "$folder_name" 4.2 --prefer-dist
+		cd "$folder_name/"
+		chmod -R 777 public/
+		chmod -R 777 bootstrap/
+		chmod -R 777 app/storage/
+		echo "hey ! your laravel 4. set up is done open localhost/$folder_name/public"
+	else
+		echo "well Subhankar I m smart enough to find ur name as well"
+		echo "I m gonna install laravel 5.1 in ur fucking machine"
+		echo -n "what should be the project folder name? [ENTER] :"
+		read folder_name
+		if [ -n "$folder_name" ]; then
+			cd /var/www/html
+			laravel new "$folder_name"
+			cd "$folder_name/"
+			chmod -R 777 public/
+			chmod -R 777 bootstrap/
+			chmod -R 777 storage/
+			echo "$(tput setaf 1)hey ! your laravel 5.2 set up is done open localhost/$folder_name/public$(tput sgr0)"
+		else
+			cd /var/www/html
+			laravel new "fresh_laravel$timestamp"
+			cd "fresh_laravel$timestamp/"
+			chmod -R 777 public/
+			chmod -R 777 bootstrap/
+			chmod -R 777 storage/
+			echo "$(tput setaf 1)hey ! your laravel 5.2 set up is done open localhost/fresh_laravel$timestamp/public$(tput sgr0)"
+		fi
+	
+	fi
+
+else
+		echo "well Subhankar I m smart enough to find ur name as well"
+		echo "I m gonna install laravel 5.1 in ur fucking machine"
+		echo -n "what should be the project folder name? [ENTER] :"
+		read folder_name
+		if [ -n "$folder_name" ]; then
+			cd /var/www/html
+			laravel new "$folder_name"
+			cd "$folder_name/"
+			chmod -R 777 public/
+			chmod -R 777 bootstrap/
+			chmod -R 777 storage/
+			echo "$(tput setaf 1)hey ! your laravel 5.2 set up is done open localhost/$folder_name/public$(tput sgr0)"
+		else
+			cd /var/www/html
+			laravel new "fresh_laravel$timestamp"
+			cd "fresh_laravel$timestamp/"
+			chmod -R 777 public/
+			chmod -R 777 bootstrap/
+			chmod -R 777 storage/
+			echo "$(tput setaf 1)hey ! your laravel 5.2 set up is done open localhost/fresh_laravel$timestamp/public$(tput sgr0)"
+		fi
+
+fi
+	
